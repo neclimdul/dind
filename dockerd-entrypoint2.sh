72a674efc2d0ba0ee47cb8a8e9e69f760ba9f0b1
James Gilliland <jgilliland@apqc.org>
2018-03-27 10:17:30 -0500

    Lets test this MTU setup out

--- dockerd-entrypoint2.sh
+++ dockerd-entrypoint2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Wait a tick after startup for docker to create the bridge and then fix the docker bridge MTU. 
-# This might be a bit error prone and a loop checking bridge existence might work better but has its own flaws. Name process mgmt and dockerd-entrypoint2.sh failure. 
+# Wait a tick after startup for docker to create the bridge and then fix the docker bridge MTU.
+# This might be a bit error prone and a loop checking bridge existence might work better but has its own flaws. Name process mgmt and dockerd-entrypoint2.sh failure.
 fix_mtu() {
   sleep 2
   echo $DOCKER_MTU
