f57d10aeebcc5f37abcea4960790eacf9e6c0fd8
James Gilliland <jgilliland@apqc.org>
2018-03-27 11:21:19 -0500

    Fix recursion instead of starting entrypoint

--- 17/dockerd-entrypoint2.sh
+++ 17/dockerd-entrypoint2.sh
@@ -4,8 +4,8 @@
 # This might be a bit error prone and a loop checking bridge existence might work better but has its own flaws. Name process mgmt and dockerd-entrypoint2.sh failure.
 fix_mtu() {
   sleep 2
-  echo $DOCKER_MTU
+  echo "MTU: $DOCKER_MTU"
 }
 fix_mtu&
 
-source dockerd-entrypoint2.sh
+source dockerd-entrypoint.sh
